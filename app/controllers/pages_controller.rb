21e92f633ae85b2bb8ab91fa42d689adcdacb08b
Leah Eramo <leah.eramo@rea-group.com>
2018-09-20 18:37:59 +1000

    scss variables and routes/controller created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+
+    def home
+
+    end
+
+    def about
+
+    end
+
+    def resources
+
+    end
+
+    def contact_us
+
+    end
+
+    def blog
+
+    end
+
+    def get_involved
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: "pages#home"
+  get '/about', to: "pages#about"
+  get '/resources', to: "pages#resources"
+  get '/get_involved', to: "pages#get_involved"
+  get '/contact_us', to: "pages#contact_us"
+  get '/blog', to: "pages#blog"
+
 end
